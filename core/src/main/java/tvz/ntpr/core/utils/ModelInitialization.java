84d3a5d97964ed2285a205cd92d91f62186e3162
Tim Pavic <pavictim8@gmail.com>
2025-08-08 17:46:54 +0200

    refactoring: formatting done in initialize() instead of returned by getCurrentTime()

--- core/src/main/java/tvz/ntpr/core/utils/ModelInitialization.java
+++ core/src/main/java/tvz/ntpr/core/utils/ModelInitialization.java
@@ -3,7 +3,9 @@ package tvz.ntpr.core.utils;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
-import tvz.ntpr.core.rest.TimeAPI;
+import tvz.ntpr.core.rest.TimeApi;
+
+import java.time.format.DateTimeFormatter;
 
 import static tvz.ntpr.core.utils.WinRegistry.*;
 
@@ -11,8 +13,10 @@ import static tvz.ntpr.core.utils.WinRegistry.*;
 @NoArgsConstructor
 public class ModelInitialization {
     public static void initialize(Model model, String currentContextPath) {
-        TimeAPI timeService = new TimeAPI();
-        String[] dateTime = timeService.getCurrentTime().split("\n");
+        TimeApi timeService = new TimeApi();
+        String[] dateTime = timeService.getCurrentTime()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"))
+                .split("T");
         model.addAttribute("currentDate", dateTime[0].trim());
         model.addAttribute("currentTime", dateTime[1].trim());
         String lang = readRegistryValue(CHANGE_LANGUAGE_REGISTRY_PATH, LANGUAGE_VALUE_NAME);
