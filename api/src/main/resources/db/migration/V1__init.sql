7486cbbd57bc462f5359dee3aae66a2caf76cff4
Tim Pavic <pavictim8@gmail.com>
2025-08-08 18:12:40 +0200

    formatting

--- api/src/main/resources/db/migration/V1__init.sql
+++ api/src/main/resources/db/migration/V1__init.sql
@@ -1,50 +1,61 @@
-DROP TABLE IF EXISTS grade CASCADE;
-DROP TABLE IF EXISTS course CASCADE;
-DROP TABLE IF EXISTS professor CASCADE;
-DROP TABLE IF EXISTS student CASCADE;
-DROP TABLE IF EXISTS user CASCADE;
-CREATE TABLE course
-(
-	id                VARCHAR(36) NOT NULL,
-	name              VARCHAR(50) NOT NULL,
-	ects              INTEGER     NOT NULL,
-	professor         VARCHAR(36),
-	PRIMARY KEY (id)
+   DROP TABLE IF EXISTS `grade`
+CASCADE;
+   DROP TABLE IF EXISTS `course`
+CASCADE;
+   DROP TABLE IF EXISTS `professor`
+CASCADE;
+   DROP TABLE IF EXISTS `student`
+CASCADE;
+   DROP TABLE IF EXISTS `user`
+CASCADE;
+
+CREATE TABLE `course` (
+		   `id` VARCHAR(36) NOT NULL,
+		 `name` VARCHAR(50) NOT NULL,
+		 `ects` INTEGER     NOT NULL,
+	`professor` VARCHAR(36),
+	PRIMARY KEY (`id`)
 );
-CREATE TABLE grade
-(
-	id                VARCHAR(36) NOT NULL,
-	course            VARCHAR(36),
-	student           VARCHAR(36),
-	grade             INTEGER     NOT NULL,
-	PRIMARY KEY (id)
+CREATE TABLE `grade` (
+		   `id` VARCHAR(36) NOT NULL,
+	   `course` VARCHAR(36),
+	  `student` VARCHAR(36),
+		`grade` INTEGER     NOT NULL,
+	PRIMARY KEY (`id`)
 );
-CREATE TABLE professor
-(
-	id                VARCHAR(36)           NOT NULL,
-	first_name        VARCHAR(50)           NOT NULL,
-	last_name         VARCHAR(50)           NOT NULL,
-	authorized        BOOLEAN DEFAULT FALSE NOT NULL,
-	PRIMARY KEY (id)
+CREATE TABLE `professor` (
+			`id` VARCHAR(36)           NOT NULL,
+	`first_name` VARCHAR(50)           NOT NULL,
+	 `last_name` VARCHAR(50)           NOT NULL,
+	`authorized` BOOLEAN DEFAULT FALSE NOT NULL,
+	 PRIMARY KEY (`id`)
 );
-CREATE TABLE student
-(
-	id                VARCHAR(36) NOT NULL,
-	jmbag             VARCHAR(11) NOT NULL,
-	first_name        VARCHAR(50) NOT NULL,
-	last_name         VARCHAR(50) NOT NULL,
-	major             VARCHAR(32) NOT NULL,
-	PRIMARY KEY (id)
+CREATE TABLE `student` (
+	        `id` VARCHAR(36) NOT NULL,
+	     `jmbag` VARCHAR(11) NOT NULL,
+	`first_name` VARCHAR(50) NOT NULL,
+	 `last_name` VARCHAR(50) NOT NULL,
+		 `major` VARCHAR(32) NOT NULL,
+	 PRIMARY KEY (`id`)
 );
-CREATE TABLE user
-(
-	id                VARCHAR(36)  NOT NULL,
-	username          VARCHAR(32)  NOT NULL,
-	password          VARCHAR(255) NOT NULL,
-	role              VARCHAR(32)  NOT NULL,
-	user_uuid         VARCHAR(36),
-	PRIMARY KEY (id)
+CREATE TABLE `user` (
+	       `id` VARCHAR(36)  NOT NULL,
+	     `role` VARCHAR(32)  NOT NULL,
+	`user_uuid` VARCHAR(36),
+	 `username` VARCHAR(32)  NOT NULL,
+	 `password` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
 );
-ALTER TABLE course ADD CONSTRAINT courseProfessorConstraint FOREIGN KEY (professor) REFERENCES professor(id);
-ALTER TABLE grade ADD CONSTRAINT gradeCourseConstraint FOREIGN KEY (course) REFERENCES course(id);
-ALTER TABLE grade ADD CONSTRAINT gradeStudentConstraint FOREIGN KEY (student) REFERENCES student(id);
+
+   ALTER TABLE `course`
+ADD CONSTRAINT `courseProfessorConstraint`
+   FOREIGN KEY (`professor`)
+    REFERENCES `professor`(`id`);
+   ALTER TABLE `grade`
+ADD CONSTRAINT `gradeCourseConstraint`
+   FOREIGN KEY (`course`)
+    REFERENCES `course`(`id`);
+   ALTER TABLE `grade`
+ADD CONSTRAINT `gradeStudentConstraint`
+   FOREIGN KEY (`student`)
+    REFERENCES `student`(`id`);
--- api/src/main/resources/db/migration/V2__add-init-values.sql
+++ api/src/main/resources/db/migration/V2__add-init-values.sql
@@ -1,18 +1,90 @@
-insert into `user`(`id`, `username`, `password`, `role`, `user_uuid`)
-values ('f2941987-1f60-4ae9-a33f-a767b9d07dff', 'admin', '6700221bbe32dbc80ba9b573f57ef75d', 'ADMIN', null);
-insert into `user`(`id`, `username`, `password`, `role`, `user_uuid`)
-values ('51269c36-1da8-4154-b6c1-4c9d22613345', 'testProf', 'b3d6069bc82a64da99c5d07d16b9984b', 'PROFESSOR',
-        'b3dbda61-412e-4df8-93c3-6bc02b6f91f1');
-insert into `user`(`id`, `username`, `password`, `role`, `user_uuid`)
-values ('afa0cd75-5940-42d7-a8ca-89e8ba9320fb', 'testStud', '26566cb79d7f81196e1ceeef596840aa', 'STUDENT',
-        '20b8f744-2d28-417c-a917-1acf0ffbb386');
-insert into `student` (`id`, `jmbag`, `first_name`, `last_name`, `major`)
-values ('20b8f744-2d28-417c-a917-1acf0ffbb386', '0246108773', 'Student', 'Test', 'SOFTWARE_ENGINEERING');
-insert into `professor` (`id`, `first_name`, `last_name`)
-values ('b3dbda61-412e-4df8-93c3-6bc02b6f91f1', 'Professor', 'Test');
-insert into `course` (`ects`, `id`, `professor`, `name`)
-values (7, '4bc9f0ab-c87e-4473-a3ec-192ab8652d41',
-		'b3dbda61-412e-4df8-93c3-6bc02b6f91f1', 'Programiranje u jeziku Java');
-insert into `grade` (`grade`, `course`, `id`, `student`)
-values (5, '4bc9f0ab-c87e-4473-a3ec-192ab8652d41',
-        'd5eb0ebb-0e76-44e8-bc40-de76f19a88eb', '20b8f744-2d28-417c-a917-1acf0ffbb386');
+INSERT INTO `user` (
+                `id`,
+                `role`,
+                `user_uuid`,
+                `username`,
+                `password`
+			)
+     VALUES (
+                'f2941987-1f60-4ae9-a33f-a767b9d07dff',
+                'ADMIN',
+                null,
+                'admin',
+                '6700221bbe32dbc80ba9b573f57ef75d'
+            );
+INSERT INTO `user` (
+                `id`,
+                `role`,
+                `user_uuid`,
+                `username`,
+                `password`
+			)
+     VALUES (
+                '51269c36-1da8-4154-b6c1-4c9d22613345',
+                'PROFESSOR',
+                'b3dbda61-412e-4df8-93c3-6bc02b6f91f1',
+                'testProf',
+                'b3d6069bc82a64da99c5d07d16b9984b'
+            );
+INSERT INTO `user` (
+                `id`,
+                `role`,
+                `user_uuid`,
+                `username`,
+                `password`
+			)
+	 VALUES (
+	            'afa0cd75-5940-42d7-a8ca-89e8ba9320fb',
+	            'STUDENT',
+	            '20b8f744-2d28-417c-a917-1acf0ffbb386',
+	            'testStud',
+	            '26566cb79d7f81196e1ceeef596840aa'
+	        );
+INSERT INTO `student` (
+                `id`,
+                `jmbag`,
+                `first_name`,
+                `last_name`,
+                `major`
+			)
+	 VALUES (
+	            '20b8f744-2d28-417c-a917-1acf0ffbb386',
+	            '0246108773',
+	            'Student',
+	            'Test',
+	            'SOFTWARE_ENGINEERING'
+	        );
+INSERT INTO `professor` (
+                `id`,
+                `first_name`,
+                `last_name`
+			)
+	 VALUES (
+                'b3dbda61-412e-4df8-93c3-6bc02b6f91f1',
+	            'Professor',
+	            'Test'
+	        );
+INSERT INTO `course` (
+				`id`,
+				`name`,
+                `ects`,
+                `professor`
+			)
+	 VALUES (
+		        '4bc9f0ab-c87e-4473-a3ec-192ab8652d41',
+		        'Programiranje u jeziku Java',
+	            7,
+				'b3dbda61-412e-4df8-93c3-6bc02b6f91f1'
+	        );
+INSERT INTO `grade` (
+				`id`,
+                `course`,
+                `student`,
+				`grade`
+			)
+	 VALUES (
+		        'd5eb0ebb-0e76-44e8-bc40-de76f19a88eb',
+	            '4bc9f0ab-c87e-4473-a3ec-192ab8652d41',
+	            '20b8f744-2d28-417c-a917-1acf0ffbb386',
+			    5
+	        );
