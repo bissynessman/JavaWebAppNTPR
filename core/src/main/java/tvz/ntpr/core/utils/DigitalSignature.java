2986512f896d196d1adaf93f64c7dcef76a8961d
Tim Pavic <pavictim8@gmail.com>
2025-08-08 17:45:40 +0200

    refactoring: sign() returns File object, string literals replaced with constants

--- core/src/main/java/tvz/ntpr/core/utils/DigitalSignature.java
+++ core/src/main/java/tvz/ntpr/core/utils/DigitalSignature.java
@@ -5,6 +5,7 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.StampingProperties;
 import com.itextpdf.signatures.*;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import tvz.ntpr.core.rest.TimeApi;
 
 import java.io.File;
 import java.io.InputStream;
@@ -12,21 +13,35 @@ import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 import java.security.Security;
+import java.time.ZoneOffset;
 
 public class DigitalSignature {
     private static final String KEYSTORE_PATH = "other/ntpr_keystore.p12";
+    private static final String KEYSTORE_TYPE = "PKCS12";
+    private static final String KEY_PASSWORD = "keypass";
+    private static final String KEY_ALIAS = "certkey";
+    private static final String HASH_ALGORITHM = "SHA256";
+    private static final String PROVIDER = "BC";
 
-    public static void sign(File input, File output) {
+    private static final TimeApi timeApi = new TimeApi();
+
+    public static File sign(File input) {
+        File output = new File(input
+                .getPath()
+                .replaceFirst(
+                        "\\.tmp",
+                        "(" + timeApi.getCurrentTime().toEpochSecond(ZoneOffset.UTC) + ")"));
+
         try {
             Security.addProvider(new BouncyCastleProvider());
-            char[] keyPassword = "keypass".toCharArray();
 
             InputStream keystoreStream = DigitalSignature.class.getClassLoader().getResourceAsStream(KEYSTORE_PATH);
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+            KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
+            char[] keyPassword = KEY_PASSWORD.toCharArray();
 
             keyStore.load(keystoreStream, keyPassword);
-            PrivateKey privateKey = (PrivateKey) keyStore.getKey("certkey", keyPassword);
-            Certificate[] certChain = keyStore.getCertificateChain("certkey");
+            PrivateKey privateKey = (PrivateKey) keyStore.getKey(KEY_ALIAS, keyPassword);
+            Certificate[] certChain = keyStore.getCertificateChain(KEY_ALIAS);
 
             PdfReader reader = new PdfReader(input);
             PdfWriter writer = new PdfWriter(output);
@@ -43,7 +58,7 @@ public class DigitalSignature {
                     .setContact("tpavic2@tvz.hr");
             signer.setSignerProperties(signerProperties);
 
-            IExternalSignature signature = new PrivateKeySignature(privateKey, "SHA-256", "BC");
+            IExternalSignature signature = new PrivateKeySignature(privateKey, HASH_ALGORITHM, PROVIDER);
 
             signer.signDetached(new BouncyCastleDigest(), signature, certChain, null, null, null, 0, null);
             reader.close();
@@ -51,5 +66,7 @@ public class DigitalSignature {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return output;
     }
 }
