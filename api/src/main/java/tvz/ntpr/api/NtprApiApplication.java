21501db314ddf2e8f90780ef7b7533c112497e0e
Tim Pavic <pavictim8@gmail.com>
2025-08-10 19:25:31 +0200

    renamed api application

--- api/src/main/java/tvz/ntpr/api/NtprApiApplication.java
+++ api/src/main/java/tvz/ntpr/api/NtprApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NtprdbRestApiApplication {
+public class NtprApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NtprdbRestApiApplication.class, args);
+        SpringApplication.run(NtprApiApplication.class, args);
     }
 }
