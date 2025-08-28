4567c02942fc5e032909f96d740944c832ddc07b
Tim Pavic <pavictim8@gmail.com>
2025-08-28 04:12:14 +0200

    refactor

--- core/src/main/java/tvz/ntpr/core/utils/JsonParser.java
+++ core/src/main/java/tvz/ntpr/core/utils/JsonParser.java
@@ -7,15 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonParser {
-    private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
 
     public static <T> T parseIntoObject(String json, Class<T> clazz) throws Exception {
         if (json == null || json.isEmpty()) return null;
-        return objectMapper.readValue(json, clazz);
+        return OBJECT_MAPPER.readValue(json, clazz);
     }
 
     public static <T> List<T> parseIntoList(String json, Class<T> clazz) throws Exception {
         if (json == null || json.isEmpty()) return new ArrayList<>();
-        return objectMapper.readValue(json, objectMapper.getTypeFactory().constructCollectionType(List.class, clazz));
+        return OBJECT_MAPPER.readValue(json, OBJECT_MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
     }
 }
